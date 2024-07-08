917d205cbe89f25e4029e3c76d376d92af1456c6
kimyerak <yerak213@gmail.com>
2024-07-09 02:53:40 +0900

    clone this!!!!

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User, UserSchema } from './schemas/user.schema';
+import { UserDayTodo, UserDayTodoSchema } from './schemas/user-day-todo.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: UserDayTodo.name, schema: UserDayTodoSchema }])],
   controllers: [UsersController],
   providers: [UsersService],
 })
