421f7ba39839b08a43d64a75db546f8f01cd8090
kimyerak <yerak213@gmail.com>
2024-07-10 12:10:13 +0900

    back schema fixed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -43,6 +43,21 @@ export class UsersController {
       const dateObj = new Date(date);
       await this.usersService.deleteTodo(name, dateObj, content);
     }
+
+
+@Post('todolists/update')
+@HttpCode(200)
+async updateTodo(
+  @Body('name') name: string,
+  @Body('date') date: string,
+  @Body('content') content: string,
+  @Body('complete') complete: boolean,
+): Promise<void> {
+  const dateObj = new Date(date);
+  await this.usersService.updateTodo(name, dateObj, content, complete);
+}
+
+
 @Get('todobydate')
   async getTodosByDate(
     @Query('name') name: string,
@@ -51,6 +66,33 @@ export class UsersController {
     const dateObj = new Date(date);
     return this.usersService.getTodosByDate(name, dateObj);
   }
+
+  @Get('alltodos')
+    async getAllTodos(
+      @Query('name') name: string,
+    ): Promise<Record<string, Todo[]>> {
+      return this.usersService.getAllTodos(name);
+    }
+
+
+@Get('todobyweek')
+async getTodosByWeek(
+  @Query('name') name: string,
+  @Query('start_date') startDate: string,
+  @Query('end_date') endDate: string,
+): Promise<Todo[]> {
+  const startDateObj = new Date(startDate);
+  const endDateObj = new Date(endDate);
+  return this.usersService.getTodosByWeek(name, startDateObj, endDateObj);
+}
+@Get('todobymonth')
+async getTodosByMonth(
+  @Query('name') name: string,
+  @Query('year') year: number,
+  @Query('month') month: number,
+): Promise<Todo[]> {
+  return this.usersService.getTodosByMonth(name, year, month);
+}
 
 
 }
